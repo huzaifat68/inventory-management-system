a8e96f3528b92d765f52fc96aa1515762518f09a
Huzaifa Tahir <huzaifatahir6868@gmail.com>
2024-10-29 20:31:10 +0500

    update

--- inventory_management_system/main.py
+++ inventory_management_system/main.py
@@ -1,3 +1,13 @@
 class User:
-    def __init__(self):
-        pass
+    def __init__(self,username,password,role):
+        self.username = username
+        self.password = password
+        self.role = role 
+
+class Product:
+    def __init__(self,product_id,name,catagory,price,stock):
+        self.product_id = product_id
+        self.name = name
+        self.catagory = catagory
+        self.price = price
+        self.stock = stock
