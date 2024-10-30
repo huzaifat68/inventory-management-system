81280b84e2f2db84c4614ec350acc471c4ee14ec
Huzaifa Tahir <huzaifatahir6868@gmail.com>
2024-10-30 15:00:47 +0500

    code update

--- inventory_management_system/main.py
+++ inventory_management_system/main.py
@@ -1,8 +1,11 @@
+
+
 class User:
     def __init__(self,username,password,role):
         self.username = username
         self.password = password
         self.role = role 
+
 
 class Product:
     def __init__(self,product_id,name,catagory,price,stock):
@@ -11,3 +14,23 @@ class Product:
         self.catagory = catagory
         self.price = price
         self.stock = stock
+    
+    def stock_update(self,quantity):
+        if self.stock + quantity < 0:
+            print(f"Error: Not enough stock, current stock is {self.stock}")
+        else:
+            self.stock += quantity
+            print(f"Stock updated for {self.name}, New stock is {self.stock}")
+
+    def view_product(self):
+        return (f"ID: {self.product_id}, Name: {self.name}, Catagory: {self.catagory},"
+        f"Price: {self.price}, Stock: {self.stock}")
+            
+
+
+
+class InventoryManagementSystem:
+    def __init__(self):
+        self.user = []
+        self.product = {}
+        self.current_user = None
