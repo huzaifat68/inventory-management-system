ab8e7db843c480c6672b410f5874f08fb8e0914f
Huzaifa Tahir <huzaifatahir6868@gmail.com>
2024-10-30 15:30:01 +0500

    add comment

--- inventory_management_system/main.py
+++ inventory_management_system/main.py
@@ -1,5 +1,5 @@
 
-
+#User class for managing roles based access
 class User:
     def __init__(self,username,password,role):
         self.username = username
@@ -7,6 +7,7 @@ class User:
         self.role = role 
 
 
+# Product class for managing and creating products
 class Product:
     def __init__(self,product_id,name,catagory,price,stock):
         self.product_id = product_id
@@ -15,6 +16,7 @@ class Product:
         self.price = price
         self.stock = stock
     
+    # Method for updating stock
     def stock_update(self,quantity):
         if self.stock + quantity < 0:
             print(f"Error: Not enough stock, current stock is {self.stock}")
@@ -22,6 +24,7 @@ class Product:
             self.stock += quantity
             print(f"Stock updated for {self.name}, New stock is {self.stock}")
 
+    # Method for viewing product information
     def view_product(self):
         return (f"ID: {self.product_id}, Name: {self.name}, Catagory: {self.catagory},"
         f"Price: {self.price}, Stock: {self.stock}")
@@ -30,18 +33,20 @@ class Product:
 
 
 class InventoryManagementSystem:
-   
+   # Initialize the system , storing user, product and current user  
     def __init__(self):
         self.users = []
         self.product = {}
         self.current_user = None
 
+    # Method for adding user
     def add_user(self,username,password,role):
         user = User(username,password,role)
         self.users.append(user)
 
+    # Method for login
     def login(self):
         username = input("Enter username:")
         password = input("Enter password:")
-        
+
 
