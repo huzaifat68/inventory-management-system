27b4ea334c1302b5c5f6b62ebecc52901cda5f55
Huzaifa Tahir <huzaifatahir6868@gmail.com>
2024-11-06 12:09:54 +0500

    exceptions

--- inventory_management_system/main.py
+++ inventory_management_system/main.py
@@ -79,13 +79,15 @@ class InventoryManagementSystem:
     # Method for adding user
     def add_user(self):
         if self.current_user and self.current_user.role== "Admin":
-            username = input("\nEnter new username: ")
-            password = input("Enter new password: ")
-            role = input("Enter new role (Admin/User): ").capitalize()
-            user = User(username,password,role)
-            self.users.append(user)
-            print(f"User {username} with the role {role} added")
-
+            try:
+                username = input("\nEnter new username: ")
+                password = input("Enter new password: ")
+                role = input("Enter new role (Admin/User): ").capitalize()
+                user = User(username,password,role)
+                self.users.append(user)
+                print(f"User {username} with the role {role} added")
+            except ValueError:
+                print("ValueError: Exception Handling SE")
         else:
             print("Access denied. Users with the admin role can add users.")
 
@@ -117,52 +119,64 @@ class InventoryManagementSystem:
     # Method to edit the product
     def edit_product(self):
         if self.current_user.role == "Admin":
-            prodcut_id = int(input("\nEnter product ID: "))
-            product = self.products.get(prodcut_id)
-            if product:
-                product.name = input("Enter new product Name: ")
-                product.catagory = input("Enter new product Catagory: ")
-                product.price = input("Enter new product Price: ")
-                product.stock = input("Enter new product Stock: ")
-            else:
-                print("Product not found.")
+            try:
+                prodcut_id = int(input("\nEnter product ID: "))
+                product = self.products.get(prodcut_id)
+                if product:
+                    product.name = input("Enter new product Name: ")
+                    product.catagory = input("Enter new product Catagory: ")
+                    product.price = input("Enter new product Price: ")
+                    product.stock = input("Enter new product Stock: ")
+                else:
+                    print("Product not found.")
+            except ValueError:
+                print("ValueError: Exception Handling SE")
         else:
             print("Access denied. Users with the admin role can Edit product.")
 
 
     # Method for deleting product
     def delete_product(self):
-        if self.current_user.role == "Admin":
-            product_id = int(input("Enter product ID to delete the product: "))
-            if product_id in self.products:
-                del self.products[product_id]
-                print(f"\nProduct with the ID {product_id} has been deleted.")
+        try:
+            if self.current_user.role == "Admin":
+                product_id = int(input("Enter product ID to delete the product: "))
+                if product_id in self.products:
+                    del self.products[product_id]
+                    print(f"\nProduct with the ID {product_id} has been deleted.")
+                else:
+                    print("\nError: Product not found.")
             else:
-                print("\nError: Product not found.")
-        else:
-            print("Access denied. Users with the admin role can delete product.")
+                print("Access denied. Users with the admin role can delete product.")
+        except ValueError:
+            print("ValueError: Exception Handling SE")
 
 
     # Method for viewing inventory
     def view_inventory(self):
-        print("Current Inventory:")
-        for product in self.products.values():
-            print(product.view_product())
-            print("-" * 50)
+        try:
+            print("Current Inventory:")
+            for product in self.products.values():
+                print(product.view_product())
+                print("-" * 50)
+        except ValueError:
+            print("ValueError: Exception Handling SE")
 
 
     # Method for searching product
     def search_product(self):
-        search = input("\nEnter Product name or Category to search: ").lower()
-        product_found = [product.view_product() for product in self.products.values()
-                        if search in product.name.lower() or search in product.catagory.lower()]
-        if product_found:
-            print("Search Result:")
-            for product_info in product_found:
-                print(product_info)
-                print("-" * 50)
-        else:
-            print("Product not found.")
+        try:
+            search = input("\nEnter Product name or Category to search: ").lower()
+            product_found = [product.view_product() for product in self.products.values()
+                            if search in product.name.lower() or search in product.catagory.lower()]
+            if product_found:
+                print("Search Result:")
+                for product_info in product_found:
+                    print(product_info)
+                    print("-" * 50)
+            else:
+                print("Product not found.")
+        except ValueError:
+            print("ValueError: Exception Handling SE")
 
 
     # Method for checking stock level
@@ -183,19 +197,20 @@ class InventoryManagementSystem:
 
     # Method for adjusting stock
     def adjust_stock(self):
-        if self.current_user.role == "Admin":
-            try:
-                product_id = int(input("\nEnter product ID to adjust stock: "))
-                product = self.products.get(product_id)
-                if product:
-                    quantity = int(input("Enter quantity to add adjust stock ( '+' to add and '-' to reduce): "))
-                    product.stock_update(quantity)
-                else:
-                    print("Product not found")
-            except ValueError:
-                print("Error: Please enter a valid Product ID and Quantity.")
-        else:
-            print("Access denied. Users with the admin role can adjust product.")
+            if self.current_user.role == "Admin":
+                try:
+                    product_id = int(input("\nEnter product ID to adjust stock: "))
+                    product = self.products.get(product_id)
+                    if product:
+                        quantity = int(input("Enter quantity to add adjust stock ( '+' to add and '-' to reduce): "))
+                        product.stock_update(quantity)
+                    else:
+                        print("Product not found")
+                except ValueError:
+                    print("Error: Please enter a valid Product ID and Quantity.")
+            else:
+                print("Access denied. Users with the admin role can adjust product.")
+        
 
 
     # Method for sale
@@ -218,104 +233,127 @@ class InventoryManagementSystem:
 
     # Method for checking Users
     def check_users(self):
-        if self.current_user.role == "Admin":
-            users = self.users
+            if self.current_user.role == "Admin":
+                try:
+                    users = self.users
 
-            for user in users:
-                print(f"Username: {user.username}, Password: {user.password}, Role: {user.role}")
-                print("-" * 50)
+                    for user in users:
+                        print(f"Username: {user.username}, Password: {user.password}, Role: {user.role}")
+                        print("-" * 50)
+                except ValueError:
+                    print("ValueError: Exception Handling SE")
+            else:
+                print("Access denied. Users with the admin role can adjust product.")  
 
-        else:
-            print("Access denied. Users with the admin role can adjust product.")  
 
 
     # Method for deleting user
     def delete_user(self):
-        if self.current_user.role == "Admin":
-            username = input("Enter username to delete user: ")
-            password = input("Enter password to delete user: ")
 
-            user_to_del = None
-            for user in self.users:
-                
-                if user.username == username and user.password == password:
-                    user_to_del = user
-                    break
+            if self.current_user.role == "Admin":
+                try:
+                    username = input("Enter username to delete user: ")
+                    password = input("Enter password to delete user: ")
 
-            if user_to_del:
-                print(f"\nUser has been deleted.")
+                    user_to_del = None
+                    for user in self.users:
+                        
+                        if user.username == username and user.password == password:
+                            user_to_del = user
+                            break
 
-                self.users.remove(user_to_del)
-                
+                    if user_to_del:
+                        print(f"\nUser has been deleted.")
+
+                        self.users.remove(user_to_del)
+                        
+                    else:
+                        print("Error: User not found.")  
+                except ValueError:
+                    print("ValueError: Exception Handling SE")          
             else:
-                print("Error: User not found.")            
-        else:
-            print("Access denied. Users with the admin role can adjust product.")
+                print("Access denied. Users with the admin role can adjust product.")
+        
+        
 
 
 
 
 
 if __name__ == "__main__":
-    ims = InventoryManagementSystem()
-    
-    
-    print("Welcome to Console base Inventory Management System (IMS)")
-    while True:
-        if ims.login():
-            while True:
-                if ims.current_user.role == "Admin":
-                    print("\n1: Add User\n2: Add Product\n3: Edit Product\n4: Delete Product\n5: View Inventory\n6: Adjust Stock\n7: Record a Sale\
-                        \n8: Search Product\n9: Check Stock\n10: Check Users\n11: Delete User\n12: Logout")
-                else:
-                    print("\n1: View Inventory\n2: Recod a Sale\n3: Search Product\n4: Check Stock\n5: Logout")
+    try:
+        ims = InventoryManagementSystem()
+        print("Welcome to Console-based Inventory Management System (IMS)")
+        
+        while True:
+            try:
+                if ims.login():
+                    while True:
+                        if ims.current_user.role == "Admin":
+                            print("\n1: Add User\n2: Add Product\n3: Edit Product\n4: Delete Product\n5: View Inventory\n6: Adjust Stock\n7: Record a Sale\
+                                \n8: Search Product\n9: Check Stock\n10: Check Users\n11: Delete User\n12: Logout")
+                        else:
+                            print("\n1: View Inventory\n2: Record a Sale\n3: Search Product\n4: Check Stock\n5: Logout")
 
-                choice = input("Choose an option: ")
+                        choice = input("Choose an option: ")
 
-                if ims.current_user.role == "Admin":
-                    if choice == "1":
-                        ims.add_user()
-                    elif choice == "2":
-                      ims.add_product()
-                    elif choice == "3":
-                        ims.edit_product()
-                    elif choice == "4":
-                        ims.delete_product()
-                    elif choice == "5":
-                        ims.view_inventory()
-                    elif choice == "6":
-                        ims.adjust_stock()
-                    elif choice == "7":
-                        ims.sale()
-                    elif choice == "8":
-                        ims.search_product()
-                    elif choice == "9":
-                        ims.stock_check()
-                    elif choice == "10":
-                        ims.check_users()
-                    elif choice == "11":
-                        ims.delete_user()
-                    elif choice == "12":
-                        print("Loggin out...")
-                        ims.current_user = None
-                        break
-                    else:
-                        print("Error: Invalid Choice. Try Again")
-               
-                else:
-                    if choice == "1":
-                        ims.view_inventory()
-                    elif choice == "2":
-                        ims.sale()
-                    elif choice == "3":
-                        ims.search_product()
-                    elif choice == "4":
-                        ims.stock_check()
-                    elif choice == "5":
-                        print("Loggin out...")
-                        ims.current_user = None
-                        break
-                    else:
-                        print("Error: Invalid Choice. Try Again")
-        else:
-            print("Login failed. Try again")
+                        if ims.current_user.role == "Admin":
+                            if choice == "1":
+                                ims.add_user()
+                            elif choice == "2":
+                                ims.add_product()
+                            elif choice == "3":
+                                ims.edit_product()
+                            elif choice == "4":
+                                ims.delete_product()
+                            elif choice == "5":
+                                ims.view_inventory()
+                            elif choice == "6":
+                                ims.adjust_stock()
+                            elif choice == "7":
+                                ims.sale()
+                            elif choice == "8":
+                                ims.search_product()
+                            elif choice == "9":
+                                ims.stock_check()
+                            elif choice == "10":
+                                ims.check_users()
+                            elif choice == "11":
+                                ims.delete_user()
+                            elif choice == "12":
+                                print("Logging out...")
+                                ims.current_user = None
+                                break
+                            else:
+                                print("Error: Invalid Choice. Try Again")
+                       
+                        else:
+                            if choice == "1":
+                                ims.view_inventory()
+                            elif choice == "2":
+                                ims.sale()
+                            elif choice == "3":
+                                ims.search_product()
+                            elif choice == "4":
+                                ims.stock_check()
+                            elif choice == "5":
+                                print("Logging out...")
+                                ims.current_user = None
+                                break
+                            else:
+                                print("Error: Invalid Choice. Try Again")
+            
+            except AttributeError as e:
+                print("Error: It seems there was an issue with accessing user attributes. Please check the user settings or contact support.")
+                print(f"Debug info: {e}")
+
+            except Exception as e:
+                print("An unexpected error occurred during the login session.")
+                print(f"Debug info: {e}")
+
+    except KeyboardInterrupt:
+        print("\nProgram terminated by the user. Exiting...")
+
+    except Exception as e:
+        print("An unexpected error occurred during program initialization.")
+        print(f"Debug info: {e}")
